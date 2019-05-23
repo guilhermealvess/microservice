e826aff35a42055aefafd9c0d9b78353e4263d5f
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-05-23 16:29:38 -0300

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  console.log(req.body);
+  res.send("Hello World!");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
