e1b8c475f5df24bf855131ee8bec3d21ab1203c8
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-06-09 12:41:33 -0300

    Add cors API

--- services/app.js
+++ services/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const router = require("./router/index");
 const port = 3000;
@@ -8,13 +9,13 @@ app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", router.home);
 app.use("/aluno", router.aluno);
 app.use("/disciplina", router.disciplina);
 app.use("/professor", router.professor);
 
-
 app.listen(port, () => {
-  console.log("RUN SERVER IN PORT http://localhost:"+port+"/");
+  console.log("RUN SERVER IN PORT http://localhost:" + port + "/");
 });
